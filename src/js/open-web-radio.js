a176a9a18131b83ac4e1246a854957ccf6f2a33d
syntagmatic <kai.s.chang@gmail.com>
2013-01-18 16:28:48 -0800

    easing, tuning click

--- src/js/open-web-radio.js
+++ src/js/open-web-radio.js
@@ -23,11 +23,11 @@ var socket = io.connect('http://confit.aws.af.cm');
 
 socket.on('connect', function (data) {
   socket.emit("join_channel", stations[0].metadata);
-  console.log("connected");
+  //console.log("connected");
 });
 
 socket.on('track_change', function (data) {
-  console.log(data);
+  //console.log(data);
   $("#song_metadata h1").text(data.name || "-");
   $("#song_metadata .artist").text(data.artist || "-");
   $("#song_metadata .album").text(data.album || "-");
@@ -36,8 +36,8 @@ socket.on('track_change', function (data) {
 
 $(document).ready(function(){    
   // radio player
-	var myPlayer = $("#jquery_jplayer_1");   
-	var station = 0;   
+  var myPlayer = $("#jquery_jplayer_1");   
+  var station = 0;   
 
   // radio appearance
 
@@ -46,19 +46,19 @@ $(document).ready(function(){
   var volDegMin = 0;
   var volDegMax = 270;
 
-	myPlayer.jPlayer({
-		ready: function () {
+  myPlayer.jPlayer({
+    ready: function () {
       $(this).jPlayer("setMedia", {
         mp3: stations[station].mp3
       });
     },   
-  	swfPath: "js",
-  	supplied: "mp3"
+    swfPath: "js",
+    supplied: "mp3"
     //errorAlerts: "true",
     //warningAlerts: "true"
   }); 
    
-	var zoneSize = 100/stations.length;  
+  var zoneSize = 100/stations.length;  
    
   function bound(val) {
     if (val > 100) return 100;
@@ -74,38 +74,42 @@ $(document).ready(function(){
     .animate({
       rotate: ($("#tuning").data("value") * 3.6) + "degs"
     }, 920);
+  $('#dialer').animate({'left': ((0.5/stations.length*dialerWidth)+dialerStart)+'px'}, 920);     
 
-	function spinTuning(event) {
-		var self = $('#tuning');
+  function spinTuning(event) {
+    var self = $('#tuning');
     var value = self.data("value") - 1.6*event.move.y;
-		var pc = bound(value);       
+    var pc = bound(value);       
     self.data("value", pc);
-		var degs = (pc * 3.6) +"deg"; 
-		self.css({rotate: degs}); 
-		
-		// Check for station change
-		
-		for (i=0; i<stations.length; i++)
-		{
-			if (pc > i*zoneSize && pc <= (i+1)*zoneSize && i != station) {
-			  // Change stations  
-			  console.log('station = '+i);
-				station = i;  
-				myPlayer.jPlayer("setMedia", {
-					mp3: stations[station].mp3
-		    });
-        socket.emit("join_channel", stations[i].metadata);
-		           
-				// delay before playing or Firefox will stall for 15 seconds
-				setTimeout(function() {myPlayer.jPlayer('play');},1500);
-			}
-		}
-		
-		return pc;
-	}    
-	
-	var switchedOn = false;
-	var volume = 0;    
+    var degs = (pc * 3.6) +"deg"; 
+    self.css({rotate: degs}); 
+    
+    // Check for station change
+    
+    for (i=0; i<stations.length; i++)
+    {
+      if (pc > i*zoneSize && pc <= (i+1)*zoneSize && i != station) {
+        changeStation(i);
+      }
+    }
+    return pc;
+  }    
+
+  function changeStation(i) {
+    // Change stations  
+    //console.log('station = '+i);
+    station = i;  
+    myPlayer.jPlayer("setMedia", {
+      mp3: stations[station].mp3
+    });
+    socket.emit("join_channel", stations[i].metadata);
+           
+    // delay before playing or Firefox will stall for 15 seconds
+    setTimeout(function() {myPlayer.jPlayer('play');},1200);
+  };
+  
+  var switchedOn = false;
+  var volume = 0;    
   
   // start at spinVolumes current value
   $("#volume")
@@ -115,19 +119,19 @@ $(document).ready(function(){
     .animate({
       rotate: $("#volume").data("value") * (volDegMax - volDegMin) / 100 + volDegMin + "degs"
     }, 820);
-	
-	function spinVolume(event) {
-		var self = $('#volume');  
-	   
+  
+  function spinVolume(event) {
+    var self = $('#volume');  
+     
     var value = self.data("value") - 1.0*event.move.y;
-		var pc = bound(value);       
+    var pc = bound(value);       
     self.data("value", pc);
-		var degs = pc * (volDegMax - volDegMin) / 100 + volDegMin + "degs"; 
+    var degs = pc * (volDegMax - volDegMin) / 100 + volDegMin + "degs"; 
 
     self.css({rotate: degs});   
-		myPlayer.jPlayer("volume", pc/100);
-	}
-	
+    myPlayer.jPlayer("volume", pc/100);
+  }
+  
   // pick random static sound and play it
   var play_static = function() {
     var next_static = Math.round(5*Math.random());
@@ -138,32 +142,44 @@ $(document).ready(function(){
 
     // play static
     document.getElementById('static_' + next_static).play();
-    console.log("static");
+    //console.log("static");
   }
   var throttled_static = _.throttle(play_static, 1640);
-	
-	$('#tuning').grab({
-		onstart: function(){     
-			// dragging = true;
-		}, onmove: function(event){ 
+  
+  var tune_moved = false;
+  $('#tuning').grab({
+    onstart: function(){     
+      // dragging = true;
+      tune_moved = false;
+    }, onmove: function(event){ 
+      tune_moved = true;
       throttled_static();
-			var pc = spinTuning(event); 
-			$('#dialer').css('left',((pc*dialerWidth/100)+dialerStart)+'px');     
-		}, onfinish: function(event){
-      // do your funky thang here ...
-		}
-	});   
-	
+      var pc = spinTuning(event); 
+      $('#dialer').css('left',((pc*dialerWidth/100)+dialerStart)+'px');     
+    }, onfinish: function(event){
+      if (tune_moved) return;
+      station = (station + 1) % stations.length;
+      changeStation(station);
+      play_static();
+      $('#dialer').animate({
+        'left': (((station+0.5)/stations.length*dialerWidth)+dialerStart)+'px'
+      }, {
+        duration: 1900,
+        easing: 'easeOutBack'
+      });     
+    }
+  });   
+  
   var vol_moved = false;
   var vol_on = true;
-	$('#volume').grab({
-		onstart: function(event){     
+  $('#volume').grab({
+    onstart: function(event){     
       vol_moved = false;
-		}, onmove: function(event){   
+    }, onmove: function(event){   
       vol_moved = true;
       vol_on = true;
-			spinVolume(event);   
-		}, onfinish: function() {
+      spinVolume(event);   
+    }, onfinish: function() {
       if (vol_moved) return;
       if (vol_on) {
         vol_on = false;
@@ -185,9 +201,7 @@ $(document).ready(function(){
         myPlayer.jPlayer("volume", volume/100);
       }
     }
-	}); 
-
-
+  }); 
 
   // autoplay
   myPlayer.jPlayer("setMedia", {
